06d309e563324906acb19ff404ee8d1cf50e5d90
Deniz Şafak <39929354+denizsafak@users.noreply.github.com>
2024-03-14 01:23:02 +0300

    Add files via upload

--- run.bat
+++ run.bat
@@ -1,13 +1,30 @@
 @echo off
+setlocal enabledelayedexpansion
 set NAME=HotspotAutoLogin
 set RUN=HotspotAutoLogin.pyw
 set "requirementsFile=requirements.txt"
 set VENV_PATH=.venv
 set ACTIVATE_PATH=%VENV_PATH%\Scripts\activate
 
+:: Check if Python is installed
+python --version >nul 2>&1
+if errorlevel 1 (
+    echo Python is not installed.
+    set /p "userinp=Do you want to install Python (y/n)? "
+    if /i "!userinp!"=="y" (
+        echo Opening download link...
+        start https://www.python.org/downloads/
+        exit /b
+    ) else (
+        echo Please install Python and try again.
+        pause
+        exit /b
+    )
+)
+
 IF NOT EXIST %ACTIVATE_PATH% (
     echo Creating virtual environment...
-    virtualenv %VENV_PATH%
+    python -m venv %VENV_PATH%
 )
 
 echo Activating virtual environment...
